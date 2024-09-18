fa700da409c253cb1eafd3347d3fc66beed6991e
Anatoly <Anatoly.Laptev@lanit-tercom.ru>
2024-09-18 14:52:44 +0300

    Adds Github Actions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter as Router } from "react-router-dom";
+import * as app from "react";
+import * as path from "path";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +11,9 @@ root.render(
         <App />
     </Router>
 );
+
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
